995f1bd4f5e003c730c762987c5054c6ae8d6413
lukkanog <lukanog@hotmail.com>
2020-12-14 08:09:24 -0300

    componente reinos atualizado

--- ensino-medieval-react/src/components/Reinos.js
+++ ensino-medieval-react/src/components/Reinos.js
@@ -4,7 +4,7 @@ import ReinoNatura from "../assets/img/natura.svg";
 import ReinoLinguagens from "../assets/img/linguagens.svg";
 import ReinoHumanas from "../assets/img/humanas.svg";
 import ReinoSocio from "../assets/img/socioemocional.svg";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Reinos() {
     const [nivelLinguagens, setNivelLinguagens] = useState(0);
@@ -19,50 +19,27 @@ export default function Reinos() {
     }
 
     return (
-        // <div className="reinos">
-        //     <div className="reino linguagens">
-        //         <img src={ReinoLinguagens} alt="" className="reino-image" style={{ filter: `grayscale(${100 - getOpacity(nivelLinguagens)}%)` }} />
-        //         <p className="reino-status">{`${nivelLinguagens}%`}</p>
-        //     </div>
-        //     <div className="reino socioemocional">
-        //         <img src={ReinoSocio} alt="" className="reino-image" style={{ filter: `grayscale(${100 - getOpacity(nivelSocio)}%)` }} />
-        //         <p className="reino-status">{`${nivelSocio}%`}</p>
-        //     </div>
-        //     <div className="reino exatas">
-        //         <img src={ReinoExactus} alt="" className="reino-image" style={{ filter: `grayscale(${100 - getOpacity(nivelExatas)}%)` }} />
-        //         <p className="reino-status">{`${nivelExatas}%`}</p>
-        //     </div>
-        //     <div className="reino humanas">
-        //         <img src={ReinoHumanas} alt="" className="reino-image" style={{ filter: `grayscale(${100 - getOpacity(nivelHumanas)}%)` }} />
-        //         <p className="reino-status">{`${nivelHumanas}%`}</p>
-        //     </div>
-        //     <div className="reino natura">
-        //         <img src={ReinoNatura} alt="" className="reino-image" style={{ filter: `grayscale(${100 - getOpacity(nivelNatura)}%)` }} />
-        //         <p className="reino-status">{`${nivelNatura}%`}</p>
-        //     </div>
-        // </div>
-
         <div className="reinos">
-            <div className="reino linguagens">
+            <Link to={"/missoes"} className="reino linguagens">
                 <img src={ReinoLinguagens} alt="" className="reino-image" style={{ opacity: `${getOpacity(nivelLinguagens)}%` }} />
                 <p className="reino-status">{`${nivelLinguagens}%`}</p>
-            </div>
-            <div className="reino socioemocional">
+            </Link>
+            <Link to="/missoes" className="reino socioemocional">
                 <img src={ReinoSocio} alt="" className="reino-image" style={{ opacity: `${getOpacity(nivelSocio)}%` }} />
                 <p className="reino-status">{`${nivelSocio}%`}</p>
-            </div>
-            <div className="reino exatas">
+            </Link>
+            <Link to="/missoes" className="reino exatas">
                 <img src={ReinoExactus} alt="" className="reino-image" style={{ opacity: `${getOpacity(nivelExatas)}%` }} />
                 <p className="reino-status">{`${nivelExatas}%`}</p>
-            </div>
-            <div className="reino humanas">
+            </Link>
+            <Link to="/missoes" className="reino humanas">
                 <img src={ReinoHumanas} alt="" className="reino-image" style={{ opacity: `${getOpacity(nivelHumanas)}%` }} />
                 <p className="reino-status">{`${nivelHumanas}%`}</p>
-            </div>
-            <div className="reino natura">
+            </Link>
+            <Link to="/missoes" className="reino natura">
                 <img src={ReinoNatura} alt="" className="reino-image" style={{ opacity: `${getOpacity(nivelNatura)}%` }} />
                 <p className="reino-status">{`${nivelNatura}%`}</p>
-            </div>
+            </Link>
         </div>
     )
 }
